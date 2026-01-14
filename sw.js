a9c7fcc1d75e567f58f20b895d810916f5d6bb4a
mollyelizabeth427 <molly.elizabeth427@gmail.com>
2026-01-13 21:53:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,37 @@
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open("lvlup").then(c=>c.addAll(["./"])))
+const CACHE = "levelup-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./styles.css",
+  "./style.css",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((r) => r || caches.match("./index.html")))
+  );
 });
